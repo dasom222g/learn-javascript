503f32d5569f77e359d74564248735abea625012
dasom222g <dasom228@gmail.com>
2025-07-08 09:49:40 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log("후츠릿 구독 좋아요");
+// console.log(2 + 2);
+
+// 자바스크립트 주석: 슬러시2개
+// 변수
+const youtube = "후츠릿"; // 상수
+let comment = "구독 좋아요"; // 변수
+console.log("🚀 ~ comment:", comment);
+
+console.log("youtube", youtube);
+
+// youtube = "태요미네"; // const변수는 재할당 불가능
+
+comment = "알림설정";
+console.log("🚀 ~ comment:", comment);
